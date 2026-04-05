bbfada0f43e6b359d86ec8a9cb3d1431204b47e1
DAKSH LAKHI <dakshlakhi01@gmail.com>
2026-04-05 23:32:49 +0530

    port

--- app.js
+++ app.js
@@ -32,8 +32,8 @@ app.use(express.json());
 // Example: /api/upload
 app.use("/api", uploadRoutes);
 app.use("/api", askRoutes);
+const PORT = process.env.PORT || 5000;
 
-// Start server on port 5000
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
